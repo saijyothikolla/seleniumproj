961faf1d902dff4c8a2f5f7cf0bffbe6968284f4
Sai jyothi <jyothisareen@gmail.com>
2020-04-24 22:35:38 +0100

    login page made changes

--- src/main/java/com/qa/hubspot/page/LoginPage.java
+++ src/main/java/com/qa/hubspot/page/LoginPage.java
@@ -20,7 +20,7 @@ public class LoginPage extends BasePage {
 	By emailId = By.id("username");
 	By password = By.id("password");
 	By loginButton = By.id("loginBtn");
-	By signUpLink = By.linkText("Sign up11");
+	By signUpLink = By.linkText("Sign up");
 	By loginErrorText = By.xpath("//div[@class='private-alert__inner']");
 
 	public LoginPage(WebDriver driver) {
